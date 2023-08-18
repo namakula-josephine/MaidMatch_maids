beb5c82fe4d05ec677a1a99017db34e4d9f2e181
mugumbyabenon <mugumbyabenon03@gmail.com>
2023-08-18 17:52:32 +0300

    removed notifications error

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -13,12 +13,7 @@ class NotificationsPage extends StatefulWidget {
 
 class _NotificationsPageState extends State<NotificationsPage> {
   final _auth = FirebaseAuth.instance;
-  final Stream<QuerySnapshot> _usersStream =
-   FirebaseFirestore.instance
-              .collection('Laundry order')
-              .where('category',isEqualTo: 'Laundry')
-            //  .where('userid', isEqualTo:  '${FirebaseAuth.instance.currentUser?.uid}')
-              .snapshots();
+  
      final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('orders');
 
@@ -44,67 +39,25 @@ class _NotificationsPageState extends State<NotificationsPage> {
       backgroundColor: Styles.backgColor,
       body:StreamBuilder<QuerySnapshot>(
         stream: streamOrders(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          print(snapshot.data?.docs.first);
-           Fluttertoast.showToast(
-        msg: '${snapshot.data?.docs.length} orders',
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        backgroundColor: Colors.green,
-        textColor: Colors.white,
-      );
-      if (!snapshot.hasData) {
-              return Text('Loading...');}
-                final List<QueryDocumentSnapshot> activeUsers = snapshot.data!.docs;
-          if (snapshot.hasData) {
-           //   return Text('Loading...');
-           return    ListView.builder(
-            itemCount: activeUsers.length,
-            itemBuilder: (context, index) {
-              final userData = activeUsers[index].data() as Map<String, dynamic>;
-               var date = userData['date'];
-               if (date != null){
-                   date = DateFormat('yyyy-MM-dd').format(userData['start-time'].toDate().toLocal()).toString()+'         '+DateFormat('h:mm a').format(userData['date'].toDate()).toString();
-               } else {
-                date = '';
-               }
-              return ListTile(
-                title: Row(
-                  children: [
-                    Text(userData['category'] ?? ''),SizedBox(width: 100),
-                    ElevatedButton(onPressed: (){}, child: Text('ACCEPT')),
-                    ElevatedButton(onPressed: (){}, child: Text('DECLINE'))
-                  ],
-                ),
-                subtitle: Row(
-                  children: [
-                    Text(date ?? '',),SizedBox(width: 20),
-                    Text(userData['status'],style: TextStyle(
-                  color: userData['status']=='Pending' ? Colors.red : Colors.green,
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.bold,
-                        ),),
-                  ],
-                ),
-                // ... other UI elements for each document
-              );
-            },
-          );
-            }
-           if (snapshot.hasError) {
-          return const Text('Something went wrong');
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text("Loading");
+          return Text("Loading");
         }
-        }
-      )
-      );
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['category']),
+              subtitle: Text(data['category']),
+            );
+          }).toList(),
+        );
+      },
+    ));
   }
 }
-    
-  
-
-    
-  
